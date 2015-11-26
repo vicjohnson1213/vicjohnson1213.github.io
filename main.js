009083e90cb9f655b6939a57cf30ac58e6fd0c48
Victor Johnson <vicjohnson1213@gmail.com>
2015-11-25 20:39:53 -0700

    Mostly working page layout.

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ $(function() {
 
         var target = this.hash;
         var $target = $(target);
-
+        
         $('html, body').stop().animate({
-            scrollTop: ($target.offset().top - 90)
+            scrollTop: ($target.offset().top - 80)
         }, 1000, 'easeInOutCubic')
     });
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= ($('#about-me-page').offset().top - 70)) {
+        if (scroll >= ($('#about-me-page').offset().top - 90)) {
             $('.menu').addClass('dark');
+            $('.arrow-container').addClass('hidden');
         } else {
             $('.menu').removeClass('dark');
+            $('.arrow-container').removeClass('hidden');
         }
     })
 });

3c3528258098749b1857ea56297021408f0c2628
Victor Johnson <vicjohnson1213@gmail.com>
2015-11-28 05:10:48 -0700

    Started working on portfolio page.

--- main.js
+++ main.js
@@ -7,22 +7,45 @@ $(function() {
 
         $('html, body').stop().animate({
             scrollTop: ($target.offset().top)
-        }, 1000, 'easeInOutCubic')
+        }, 1000, 'easeInOutCubic');
     });
 
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= ($('#about-me-page').offset().top - 70)) {
-            $('.nav-menu').addClass('dark');
-            $('.media-icons').addClass('dark');
-            $('.navbar-bg').addClass('dark');
-            $('.arrow-container').addClass('hidden');
-        } else {
-            $('.nav-menu').removeClass('dark');
-            $('.media-icons').removeClass('dark');
-            $('.navbar-bg').removeClass('dark');
-            $('.arrow-container').removeClass('hidden');
-        }
-    })
+    updateScroll();
+    $(window).scroll(updateScroll);
 });
+
+function updateScroll() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll < ($('#about-me-page').offset().top - 40)) {
+        // home page
+        $('.nav-menu').removeClass('dark');
+        $('.media-icons').removeClass('dark');
+        $('.navbar-bg').removeClass('dark');
+        $('.arrow-container').removeClass('hidden');
+
+        $('#home-btn').addClass('active');
+        $('#about-me-btn').removeClass('active');
+        $('#portfolio-btn').removeClass('active');
+    } else if (scroll < ($('#portfolio-page').offset().top - 40)) {
+        // about me page
+        $('.nav-menu').addClass('dark');
+        $('.media-icons').addClass('dark');
+        $('.navbar-bg').addClass('dark');
+        $('.arrow-container').addClass('hidden');
+
+        $('#home-btn').removeClass('active');
+        $('#about-me-btn').addClass('active');
+        $('#portfolio-btn').removeClass('active');
+    } else {
+        // portfolio page
+        $('.nav-menu').removeClass('dark');
+        $('.media-icons').removeClass('dark');
+        $('.navbar-bg').removeClass('dark');
+        $('.arrow-container').addClass('hidden');
+
+        $('#home-btn').removeClass('active');
+        $('#about-me-btn').removeClass('active');
+        $('#portfolio-btn').addClass('active');
+    }
+}
